e7f11f8a7748c0d1e9dcd27a8cfa53e77b95cae5
EthanMRichards <richa684@purdue.edu>
2025-11-23 22:33:12 -0500

    updated pin numbers

--- src/initializations.h
+++ src/initializations.h
@@ -1,27 +1,35 @@
 // dac pins
-#define DAC_SCK 2
-#define DAC_TX 3
-#define DAC_LR 1
-#define DAC_SDA 4
-#define DAC_SCL 5
-// spi 1 pins
-#define SD_SCK 10
-#define SD_TX 11
+#define DAC_SCK 6
+#define DAC_TX 7
+#define DAC_LR 47
+#define DAC_SDA 16
+#define DAC_SCL 9
+#define DAC_RST 17
+
+// sd pins
+#define SD_SCK 14
+#define SD_TX 15
 #define SD_RX 12
 #define SD_CSn 13
 // will be changed as display pins will likely be on opposite side of the chip from sd pins
-#define DISPLAY_SCK 10
-#define DISPLAY_TX 11
-#define DISPLAY_CSn 14
+#define DISPLAY_SCK 42
+#define DISPLAY_TX 43
+#define DISPLAY_CSn 46
+#define DISPLAY_RST 45
+#define DISPLAY_DC 44
 
 // button pins
-#define SELECT_PIN 15
-#define BACK_PIN 16
+#define BACK_PIN 39
 
 // rotary encoder pins
-#define ENC_A 17
-#define ENC_B 18
-#define ENC_SW 19
+#define ENC_A 37
+#define ENC_B 38
+#define ENC_SW 40
+
+// EEPROM pins
+
+#define EE_SCL 19
+#define EE_SDA 18
 
 void timer_isr();
 void timer_init();
